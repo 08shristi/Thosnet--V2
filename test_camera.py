0cfce8884c4db5d165b227a5e9290f1f38b71a97
08shristi <shristipatwa8@gmail.com>
2026-06-16 17:29:25 +0530

    Add files via upload

---
+++ test_camera.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Camera not working ❌")
+    exit()
+
+print("Camera working ✅ Press ESC to exit")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("Test Camera", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
